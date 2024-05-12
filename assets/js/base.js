f508b22b0559f7c00aa6be797942faec1b348a07
coyotesss <coyotesss@yandex.com>
2024-05-12 19:13:35 +0800

    optimize searcher

--- assets/js/base.js
+++ assets/js/base.js
@@ -1,186 +1,256 @@
-const eleSelectType = document.querySelector('#select-type');
-const eleSearchInput = document.querySelector('#search-box input');
-const eleSearchButton = document.querySelector('#search-box button');
-const eleContainer = document.querySelector('#container');
-const eleMaterials = document.querySelector('#materials');
-const eleMaterialContainers = document.querySelectorAll('.material-container');
-const eleNoResult = document.querySelector('#no-result');
-const eleLoading = document.querySelector('#loading');
-const eleModal = document.querySelector('#modal');
-const eleModalButton = document.querySelector('#modal button');
-
-
-function searchMaterial() {
-    let materialContainersCount = eleMaterialContainers.length;
-    let typeChinese = false;
-    
-    eleSelectType.addEventListener('change', () => {
-        displayLoading();
-        let keywords = eleSearchInput.value;
-        setTimeout(() => displayMaterialContainer(keywords), 100);
-        setTimeout(hideLoading, 100);
-    })
-
-    eleSearchButton.addEventListener('pointerdown', () => {
-        displayLoading();
-        let keywords = eleSearchInput.value;
-        setTimeout(() => displayMaterialContainer(keywords), 100);
-        setTimeout(hideLoading, 100);
-    })
-
-    document.addEventListener("keyup", (e) => {
-      if (e.key === "Enter") {
-        displayLoading();
-        let keywords = eleSearchInput.value;
-        setTimeout(() => displayMaterialContainer(keywords), 100);
-        setTimeout(hideLoading, 100);
-      }
-    })
-
-    function displayLoading() {
-        eleLoading.style.display = 'block';
-        eleContainer.style.filter = 'blur(10px)';
-    }
-
-    function hideLoading() {
-        eleLoading.style.display = 'none';
-        eleContainer.style.filter = 'blur(0px)';
-    }
-
-
-    function displayNoResult() {
-        eleNoResult.style.display = 'block';
-    }
-
-    function hideNoResult() {
-        eleNoResult.style.display = 'none';
-    }
-
-    function displayMaterialContainer(keywords='') {
-        let count = 0
-        let selectType = eleSelectType.value;
-        Array.from(eleMaterialContainers).forEach((ele) => {
-            if (selectType === 'all') {
-                ele.classList.remove('hide');
-            } else {
-                let materialType = ele.querySelector('.type').innerText;
-                if (materialType === selectType) {
-                    ele.classList.remove('hide');
-                } else {
-                    ele.classList.add('hide');
-                }
-            }
-            if (keywords !== '') {
-                let materialName = ele.querySelector('.name .info').innerText;
-                let materialAuthor = ele.querySelector('.author .info').innerText;
-                if (!materialName.includes(keywords) && !materialAuthor.includes(keywords)) {
-                    ele.classList.add('hide');
-                }
-            }
-        })
-        let hiddenMaterialContainersCount = document.querySelectorAll('.material-container.hide').length;
-        if (hiddenMaterialContainersCount === materialContainersCount) {
-            displayNoResult();
-        } else {
-            hideNoResult();
-        }
-        eleMaterials.scrollIntoView();
-    }
-}
-
-
-function displayAbout() {
-    let display = false;
-    let eleAboutButton = document.querySelector('#about-button');
-    let eleAboutContent = document.querySelector('#about-content');
-    let eleMaterials = document.querySelector('#materials');
-    eleAboutButton.addEventListener('pointerdown', () => {
-        eleAboutContent.style.display = 'block';
-        eleContainer.style.filter = 'blur(10px)';
-        display = true;
-    })
-    eleMaterials.addEventListener('pointerdown', () => {
-        if (display) {
-            eleAboutContent.style.display = 'none';
-            eleContainer.style.filter = 'blur(0px)';
-        }
-    })
-            
-}
-
-
-function adjustSearchBoxLength() {
-    if (/Android|webOS|iPhone|iPad|iPod|BlackBerry/i.test(navigator.userAgent)) {
-        let eleHeader = document.querySelector('#header');
-        let eleHeaderLeft = document.querySelector('#header-left');
-        let eleSelectType = document.querySelector('#select-type');
-        let eleSearchButton = document.querySelector('#search-button');
-        let eleAboutButton = document.querySelector('#about-button');
-        let eleInput = document.querySelector('#search-box input');
-        let width = `${eleHeader.clientWidth - eleHeaderLeft.clientWidth - eleSelectType.clientWidth - eleSearchButton.clientWidth - eleAboutButton.clientWidth - 30}px`;
-        eleInput.style.width = width;
-    }
-}
-
-
-function setNewBanner() {
-    let timeStamp = Math.floor(new Date().getTime() / 1000);
-    let eleMaterialContainers = document.querySelectorAll('.material-container');
-    Array.from(eleMaterialContainers).forEach((ele) => {
-        let utime = ele.getAttribute('data-utime');
-        let timeGap = timeStamp - utime;
-        if (timeGap < 259200) {
-            ele.classList.add('new');
-        }
-    })
-}
-
-
-function goHome() {
-    let eleHeader = document.querySelector('#header-left');
-    eleHeader.addEventListener('pointerdown', () => window.location.href='/');
-}
-
+import { data } from "./data.js"
 
 function customScrollbar() {
     let { 
         OverlayScrollbars, 
-        ScrollbarsHidingPlugin, 
-        SizeObserverPlugin, 
-        ClickScrollPlugin  
     } = OverlayScrollbarsGlobal;
-    OverlayScrollbars(document.querySelector('#container'), {
-        overflow: {
-            x: 'hidden',
-        },
+    document.querySelectorAll('[data-overlayscrollbars-initialize]').forEach((ele) => {
+        if (ele.classList.value === "modal") {
+            OverlayScrollbars(ele, {
+                scrollbars: {
+                    visibility: 'hidden',
+                }
+            })
+        } else {
+            OverlayScrollbars(ele, {
+                scrollbars: {
+                    visibility: "visible",
+                }
+            });
+        }
     });
 }
 
-function setContainerHeight() {
-    let height = window.innerHeight;
-    eleContainer.style.height = (height - 60) + 'px';
+function setNewBanner(ele) {
+    let timeStamp = Math.round(new Date().getTime() / 1000);
+    let utime = ele.getAttribute('data-utime');
+    let timeGap = timeStamp - utime;
+    if (timeGap < 259200) {
+        ele.classList.add('new');
+    }
 }
 
-function displayModal() {
-  eleModal.style.display = 'block';
-  setTimeout(() => {
-    eleModal.classList.add('animate');
-    eleContainer.style.filter = 'blur(10px)';
-  }, 100)
-  eleModalButton.addEventListener('click', () => {
-    eleModal.classList.remove('animate');
-    setTimeout(() => {
-      eleContainer.style.filter = 'blur(0px)';
-      eleModal.style.display = 'none';
-    }, 800);
-  })
+
+function blinkButton() {
+    let eles = document.querySelectorAll("button");
+    [...eles].forEach((ele) => {
+        let bgColor = window.getComputedStyle(ele).backgroundColor;
+        ele.addEventListener("pointerdown", () => {
+            ele.style.backgroundColor = "#fff";
+            setTimeout(() => {
+                ele.style.backgroundColor = bgColor;
+            }, 100);
+        })
+    })
 }
 
-searchMaterial();
-displayAbout();
-adjustSearchBoxLength()
-setNewBanner();
-goHome();
+function displayNoResult() {
+    let eleNoResult = document.querySelector("#no-result");
+    eleNoResult.style.display = 'block';
+}
+
+function hideNoResult() {
+    let eleNoResult = document.querySelector("#no-result");
+    eleNoResult.style.display = 'none';
+}
+
+function toggleAbout() {
+    let display = false;
+    let eleAboutButton = document.querySelector('#about-button');
+    let eleAboutContent = document.querySelector('.about');
+    let eleContainer = document.querySelector('#materials');
+    eleAboutButton.addEventListener('pointerdown', () => {
+        document.querySelector('.changelog').style.display = "none";
+        if (window.getComputedStyle(document.querySelector(".modal")).display === "none") {
+            eleAboutContent.style.display = 'block';
+            eleContainer.style.filter = 'blur(10px)';
+            display = true;
+        }
+    })
+    eleContainer.addEventListener('pointerdown', () => {
+        if (display) {
+            eleAboutContent.style.display = 'none';
+            eleContainer.style.filter = 'blur(0px)';
+        }
+    })    
+}
+
+function toggleChangelog() {
+    let display = false;
+    let eleChangelogButton = document.querySelector('#changelog-button');
+    let eleChangelogContent = document.querySelector('.changelog');
+    let eleContainer = document.querySelector('#materials');
+    eleChangelogButton.addEventListener('pointerdown', () => {
+        document.querySelector('.about').style.display = "none"
+        if (window.getComputedStyle(document.querySelector(".modal")).display === "none") {
+            eleChangelogContent.style.display = 'block';
+            eleContainer.style.filter = 'blur(10px)';
+            display = true;
+        }
+    })
+    eleContainer.addEventListener('pointerdown', () => {
+        if (display) {
+            eleChangelogContent.style.display = 'none';
+            eleContainer.style.filter = 'blur(0px)';
+        }
+    })    
+}
+
+function closeModalListener() {
+    let eleMaterials = document.querySelector("#materials");
+    let eleModal = document.querySelector(".modal");
+    let eleModalButton = document.querySelector(".modal button");
+    eleModalButton.addEventListener("pointerdown", () => {
+        eleModal.style.animation = "fadeOut 1s ease";
+        eleMaterials.style.animation = "blurOut 1s ease-in-out";
+        setTimeout(() => {
+            eleModal.style.display = "none";
+            eleMaterials.style.filter = "blur(0)";
+        }, 1000);
+    })
+}
+
+
+function dynamicLoadMaterial() {
+    // let materialData = data.content;
+    let materialData = data.content.sort((b, a) => a.utime - b.utime);
+    let filteredMaterialData = materialData;
+    let eleMaterials = document.querySelector("#materials");
+    let loadedAmount = 0;
+    let perLoadAmount = 6;
+
+    loadMaterial(30);
+
+    searchMaterialListener();
+    continueLoadListener();
+
+    function getType(mData) {
+        let typeEngArray = data.metadata.type;
+        let typeChiArray = data.metadata.type_chinese;
+        return typeChiArray[typeEngArray.indexOf(mData.type)][0];
+    }
+
+    function initSearch() {
+        while (eleMaterials.firstChild) {
+            eleMaterials.removeChild(eleMaterials.firstChild);
+        }
+        filteredMaterialData = [];
+        loadedAmount = 0;
+        hideNoResult();
+        eleMaterials.scrollIntoView();
+    }
+
+    function searchMaterial(type) {
+        console.log(type);
+        let eleSearchInput = document.querySelector(".searcher > input");
+        let searchTerm = eleSearchInput.value;
+        materialData.forEach((data) => {
+            if (type === "all") {
+                if ((data?.author ?? "").includes(searchTerm) || data.name.includes(searchTerm)) {
+                    filteredMaterialData.push(data);
+                }
+            } else {
+                if (data.type === type && ((data?.author ?? "").includes(searchTerm) || data.name.includes(searchTerm))) {
+                    filteredMaterialData.push(data);
+                }
+            }
+        })
+        if (filteredMaterialData.length === 0) {
+            displayNoResult();
+        }
+    }
+
+    function loadMaterial(amount) {
+        for (let i=0; i<amount; i++) {
+            if (loadedAmount < filteredMaterialData.length) {
+                let mData = filteredMaterialData[loadedAmount];
+                let materialType = getType(mData);
+                let eleMaterial = 
+                `<div class='material-container' data-utime=${mData.utime}>
+                    <div class='material ${mData.type}'>
+                        <div class='material-image'>
+                            <img src="${mData.cover_url}" loading='lazy' />
+                        </div>
+                        <div class='material-info'>
+                            <div class='type' style='display:none'>${mData.type}</div>
+                            <div class='type'>
+                                <span>💼类型：</span>
+                                <span class='info' title="${materialType}">${materialType ?? '🍼🍹'}</span>
+                            </div>
+                            <div class='author'>
+                                <span>👩‍🎓作者：</span>
+                                <span class='info' title="${mData.author}">${mData.author || '🏃🏃‍♀️'}</span>
+                            </div>
+                            <div class='year'>
+                                <span>📅日期：</span>
+                                <span class='info' title="${mData.year}">${mData.year ?? '🍦🍮'}</span>
+                            </div>
+                            <div class='pages'>
+                                <span>📃页数：</span>
+                                <span class='info' title="${mData.pages}">${mData.pages ?? '🍼🍹'}</span>
+                            </div>
+                        </div>
+                    </div>
+                    <div class='material-name'>《${mData.name}》</div>
+                    <div class='material-modal'>
+                        <div class='urls'>
+                            <span>🔑下载:</span>
+                            <a href="${mData.file_url}" target='_blank'>yukaidi</a>
+                        </div>
+                    </div>
+                </div>`
+                eleMaterials.insertAdjacentHTML('beforeend', eleMaterial);
+                setNewBanner(document.querySelector("#materials .material-container:last-child"));
+                loadedAmount ++;
+            }
+        }
+    }
+
+
+    function searchMaterialListener() {
+        let eleSelect = document.querySelector(".searcher > select");
+        let eleSearchButton = document.querySelector(".searcher > button");
+
+        document.addEventListener("keyup", (e) => {
+            let type = eleSelect.value;
+            if (e.key === "Enter" && document.activeElement === document.querySelector(".searcher > input")) {
+              initSearch();
+              searchMaterial(type);
+              loadMaterial(30);
+            }
+        })
+    
+        eleSearchButton.addEventListener("pointerdown", () => {
+            let type = eleSelect.value;
+            initSearch();
+            searchMaterial(type);
+            loadMaterial(30);
+        });
+
+        eleSelect.addEventListener("change", () => {
+            let type = eleSelect.value;
+            initSearch();
+            searchMaterial(type);
+            console.log(filteredMaterialData);
+            loadMaterial(30);
+        })
+    }
+
+    function continueLoadListener() {
+        window.addEventListener("wheel", () => {
+            let eleLastMaterial = document.querySelector(".material-container:last-child");
+            let eleLastMaterialTop = eleLastMaterial.getBoundingClientRect().top;
+            let windowHeight = window.innerHeight;
+            if (eleLastMaterialTop - windowHeight < 100) {
+            loadMaterial(perLoadAmount);
+            }
+        })
+    }
+}
+
+toggleAbout();
+toggleChangelog();
+closeModalListener();
 customScrollbar();
-setContainerHeight();
-displayModal();
+blinkButton();
+dynamicLoadMaterial();
